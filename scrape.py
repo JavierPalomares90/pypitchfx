ea86ab487896420ab59123f3fe50793a4a880b52
Javier Palomares <javier.palomares.90@gmail.com>
2019-05-18 17:57:22 -0500

    More partial impl

--- scrape.py
+++ scrape.py
@@ -2,23 +2,59 @@
 import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
+import gids as gds
+import argparse
+
 '''
 Python tool to scrape pitchf/x data from mlb's website.
 Inspired by pitchfx package written in R
 Author: Javier Palomares
 '''
 
-def makeUrls(start,end,gids=None):
+def gids2urls(gids):
+    root = "http://gd2.mlb.com/components/game/"
+    lea
+
+
+def makeUrls(start=None,end=None,gids=None):
     root = "http://gd2.mlb.com/components/game/mlb/"
     if gids is None:
         if start is None or end is None:
             raise Exception("Need to specify start or end")
-    start = 
+        gids = gds.get_gids()
+        subset_gids = get_subset_gids(gids,start,end)
+        return gids2urls(subset_gids)
+        
+        
+def get_subset_gids(gids,first,last):
+    list = []
+    for gid in gids:
+        elements = gid.split('-')
+        # TODO get proper subset
+        if(True):
+            list.append(gid)
+    return list
+        
 
-
-def scrape(start,end,game_ids,suffix="inning/inning_all.xml",db_connection):
+def scrape(start,end,game_ids=None,suffix="inning/inning_all.xml",db_connection=None):
     if game_ids is None:
         game_dir = makeUrls(start,end)
     else:
         game_dir = makeUrls(gids=game_ids)
+    pass
     
+def get_args():
+    parser = argparse.ArgumentParser(description='Scrape data')
+    parser.add_argument('-s','--start')
+    parser.add_argument('-e','--end')
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = get_args()
+    start = args.start
+    end = args.end
+    scrape(start,end)
+
+if __name__=="__main__":
+    main()
