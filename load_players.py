ee780132bb2d44452bd3e41c4753f964b0f8eac6
Javier Palomares <javier.palomares.90@gmail.com>
2019-05-26 12:09:58 -0500

    load_players. this should go into the other repo

--- load_players.py
+++ load_players.py
@@ -1,9 +1,35 @@
 import feather
+from sqlalchemy import create_engine
+import psycopg2
+
+BATCH_SIZE = 5000;
+
+INSERT_PLAYERS = """INSERT INTO player(player_id,full_name)
+VALUES({player_id},{full_name})
+"""
+
+def get_conn():
+    conn = psycopg2.connect(user='postgres', password='J5Oou5wrkGgN4I7G',
+                        host='localhost', port='5432')
+    return conn 
+
+def insert_player(cursor,player):
+    player_id = player['id']
+    full_name = player['full name']
+    sql = INSERT_PLAYERS.format(player_id=player_id,full_name=full_name)
+    cursor.execute(sql)
+
 def load_players():
     path = "data/py_players.feather"
     df = feather.read_dataframe(path)
-
-
+    conn = get_conn()
+    cursor = conn.cursor()
+    i = 0
+    for player in df.rows:
+        insert_player(cursor,player)
+        if(i % BATCH_SIZE == 0):
+            conn.commit()
+    cursor.close()
 def main():
     load_players()
 
