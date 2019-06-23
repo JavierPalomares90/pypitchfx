14a67c94b594962048fcc88ec10e82fc59c25355
Javier Palomares <javier.palomares.90@gmail.com>
2019-06-22 19:55:12 -0500

    Need to finish impl of creating tables

--- pypitchfx/load/create_tables.py
+++ pypitchfx/load/create_tables.py
@@ -1,5 +1,39 @@
 from sqlalchemy.sql import text
 
-def create_tables(conn):
-    #TODO: Complete impl
+def create_game_table(conn):
     pass
+
+def  create_inning_table(conn):
+    pass
+
+def create_half_inning_table(conn):
+    pass
+
+def create_at_bat_table(conn):
+    pass
+
+def create_pitch_table(conn):
+    pass
+
+def create_runner_table(conn):
+    pass
+
+def create_pickoff_table(conn):
+    pass
+
+def create_action_table(conn):
+    pass
+
+def create_game_player_table(conn):
+     pass
+
+def create_tables(conn):
+    create_game_table(conn)
+    create_inning_table(conn)
+    create_half_inning_table(conn)
+    create_at_bat_table(conn)
+    create_pitch_table(conn)
+    create_runner_table(conn)
+    create_pickoff_table(conn)
+    create_action_table(conn)
+    create_game_player_table(conn)
