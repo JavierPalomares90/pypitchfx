fb29058e937c8bca893354c020fa242c6f0cb423
Javier Palomares <javier.palomares.90@gmail.com>
2019-07-13 14:10:37 -0500

    Also add sqlalchemy to requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pypitchfx',
-      version='0.8',
+      version='0.9',
       description='Tool for parsing mlb gameday data',
       url='http://github.com/JavierPalomares90/pypitchfx',
       author='Javier Palomares',
@@ -10,6 +10,7 @@ setup(name='pypitchfx',
       packages=['pypitchfx'],
       install_requires=[
           'beautifulsoup4',
-          'lxml'
+          'lxml',
+	  'sqlalchemy'
       ],
       zip_safe=False)
