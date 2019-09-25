61599dd738a36402be0dda4a7922bc66f1e009a5
Javier Palomares <javier.palomares.90@gmail.com>
2019-09-24 23:18:45 -0500

    Add submodels, update version

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='pypitchfx',
-      version='1.0.1',
+      version='1.0.4',
       description='Tool for parsing mlb gameday data',
       url='http://github.com/JavierPalomares90/pypitchfx',
       author='Javier Palomares',
       author_email='javier.palomares.90@gmail.com',
       license='MIT',
-      packages=['pypitchfx'],
+      packages=['pypitchfx','pypitchfx.gameday_model','pypitchfx.load'],
       install_requires=[
           'beautifulsoup4',
           'lxml',
